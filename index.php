fa5ff61754751361a7f3935b80ea7451f91f6c27
Eva <evulya_yakovlev@mail.ru>
2023-11-28 00:11:10 +0700

    #

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+echo "Hello PHP!"; 
+$name = "<h1>John</h1>";
+echo $name;
+echo "My name is $name";
+$lastname = 'Black';
+echo $name." ".$lastname;
+$x =23;
+echo "<p>$x</p>";
+$sum = $x + $y;
+echo "<p>$sum</p>";
+
+$variableName = "lastname";
+echo $$variableName;
+?>
